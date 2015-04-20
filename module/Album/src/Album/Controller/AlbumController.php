b0eb2e8a31feafb3d7d84de644ca14c44f5f1feb
xitrumuit1991 <tranvokhoinguyen@gmail.com>
2015-04-20 11:38:19 +0700

    new

---
+++ module/Album/src/Album/Controller/AlbumController.php
@@ -0,0 +1,32 @@
+<?php
+namespace Album\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+//
+//Cach goi 1 action o ngoai View:
+//Album\Controller\AlbumController::indexAction =>url: http://zf2-tutorial.localhost/album
+//Album\Controller\AlbumController::addAction =>url: http://zf2-tutorial.localhost/album/add
+//
+
+class AlbumController extends AbstractActionController
+{
+	public function indexAction()
+	{
+
+	}
+	public function addAction()
+	{
+		
+	}
+	public function editAction()
+	{
+		
+	}
+	public function deleteAction()
+	{
+		
+	}
+}
+?>
